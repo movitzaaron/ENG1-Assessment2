082e3e6fa1def4898981f4db9f3eb16ff9440a5f
movitzaaron <noraaztivom@gmail.com>
2024-11-29 18:28:41 +0000

    Created file where GameLogic will live

---
+++ core/src/main/java/com/spacecomplexity/longboilife/game/utils/GameLogic.java
@@ -0,0 +1,6 @@
+package com.spacecomplexity.longboilife.game.utils;
+
+public class GameLogic {
+    // This will take everything that GameScreen does logically and does it in here instead.
+    // This is to make sure that we can test the rest of the code easier by being able to create a headless application.
+}

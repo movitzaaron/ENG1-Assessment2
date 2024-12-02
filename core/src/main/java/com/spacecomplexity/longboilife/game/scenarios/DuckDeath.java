86acf5e04b8f2e0260a7f35b26f87070301f66aa
jyoung <jobshamandjam@gmail.com>
2024-12-02 16:04:18 +0000

    Created DuckDeath class

---
+++ core/src/main/java/com/spacecomplexity/longboilife/game/scenarios/DuckDeath.java
@@ -0,0 +1,18 @@
+package com.spacecomplexity.longboilife.game.scenarios;
+
+
+import com.spacecomplexity.longboilife.game.globals.GameState;
+import com.spacecomplexity.longboilife.game.utils.EventHandler;
+
+public class DuckDeath {
+    private static final DuckDeath duckDeath = new DuckDeath();
+
+    public static void begin(){
+        // do something
+    }
+
+    public static void end(){
+        // do something
+    }
+
+}

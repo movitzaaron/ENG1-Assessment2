462bd721a029942e88ba36f3fe384c1c3b3805a2
jyoung <jobshamandjam@gmail.com>
2024-12-03 15:49:59 +0000

    Created duckdeath class

--- core/src/main/java/com/spacecomplexity/longboilife/game/scenarios/DuckDeath.java
+++ core/src/main/java/com/spacecomplexity/longboilife/game/scenarios/DuckDeath.java
@@ -1,13 +1,38 @@
 package com.spacecomplexity.longboilife.game.scenarios;
 
+import com.spacecomplexity.longboilife.game.utils.EventHandler;
+import com.spacecomplexity.longboilife.game.utils.Timer;
 
+/**
+ * Singleton class to contain the duck death timer and relevant begin and end functions.
+ */
 public class DuckDeath {
+    public static final DuckDeath duckDeath = new DuckDeath();
+
+    private final Timer timer;
+
+    private DuckDeath() {
+        timer = new Timer();
+    }
+
     public static void begin(){
         // do something
+        System.out.println("Duck Death has occurred");
     }
 
     public static void end(){
         // do something
     }
+
+    /**
+     * Get the singleton instance of the {@link DuckDeath} class.
+     *
+     * @return The single {@link DuckDeath} class.
+     */
+
+    public static DuckDeath getDuckDeath() {return duckDeath;}
+
+    public Timer getTimer() {return timer;}
+
 
 }

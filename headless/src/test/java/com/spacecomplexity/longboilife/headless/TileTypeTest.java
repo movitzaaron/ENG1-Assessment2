6f29d67048a5fe8dcb9fa3a186727ef15d0e9e1b
Sky <jeanniemayhainesbass@outlook.com>
2024-12-06 12:32:05 +0000

    TileType tests

---
+++ headless/src/test/java/com/spacecomplexity/longboilife/headless/TileTypeTest.java
@@ -0,0 +1,39 @@
+package com.spacecomplexity.longboilife.headless;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.spacecomplexity.longboilife.game.tile.Tile;
+import com.spacecomplexity.longboilife.game.tile.TileType;
+import com.spacecomplexity.longboilife.game.utils.Vector2Int;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+/**
+ * Unit tests for the TileType enum.
+ */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class TileTypeTest extends AbstractHeadlessGdxTest{
+
+    @BeforeAll
+    public void setUp() {}
+
+    @AfterAll
+    public void tearDown() {}
+
+    @Test
+    @DisplayName(" Test TileType enum constants and their properties. ")
+    public void testTileTypeProperties() {
+
+        // Test GRASS
+        TileType grass = TileType.GRASS;
+        assertEquals("tiles/grass.png", grass.getTexturePath(), "Grass texture mismatch.");
+        assertTrue(grass.isNaturallyBuildable(), "Grass tile type should be naturally buildable.");
+
+        // Test WATER
+        TileType water = TileType.WATER;
+        assertEquals("tiles/water.png", water.getTexturePath(), "Water texture mismatch.");
+        assertFalse(water.isNaturallyBuildable(), "Water tile type should not be naturally buildable.");
+    }
+
+}

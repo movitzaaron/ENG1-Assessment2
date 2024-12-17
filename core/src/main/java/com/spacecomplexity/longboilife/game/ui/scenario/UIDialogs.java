b3a63056a42764ab4177e534c9fb476ed9837276
Prodeerom <obelam@icloud.com>
2024-12-17 16:45:42 +0000

    Tutorial and outcomes finished. One issue to note is that on replay no events run

--- core/src/main/java/com/spacecomplexity/longboilife/game/ui/scenario/UIDialogs.java
+++ core/src/main/java/com/spacecomplexity/longboilife/game/ui/scenario/UIDialogs.java
@@ -55,14 +55,13 @@ public class UIDialogs extends UIElement {
             @Override
             protected void result(Object object) {
                 System.out.println("Dialog choice: " + object);
-                if (object.equals(0)){
+                if (object.equals(1)){
                     //Enables the tutorial
-                    GameState.getState().money += 800000;
-
+                    tutorialDialogOutcome();
                 }
-
-                EventHandler.getEventHandler().callEvent(EventHandler.Event.RESUME_GAME);
-                // Resumes the game,
+                else {
+                    EventHandler.getEventHandler().callEvent(EventHandler.Event.RESUME_GAME);
+                    }
             }
         };
 
@@ -76,11 +75,43 @@ public class UIDialogs extends UIElement {
         // Add the label to the dialog's content table
         dialog.getContentTable().add(label).width(500).height(300).pad(10); // You can adjust the size
 
-        dialog.button("Enable tutorial", 0);
-        dialog.button("Disable tutorial", 1);
+        dialog.button("Disable tutorial", 0);
+        dialog.button("Enable tutorial", 1);
 
         // Show the dialog
         dialog.show(stage);
+    }
+
+    private void tutorialDialogOutcome() {
+        // Followup dialog: Tutorial explanation
+        Dialog tutorialDialog = new Dialog("Tutorial", skin) {
+            @Override
+            protected void result(Object object) {
+                // Resumes the game when "Continue" is clicked
+                EventHandler.getEventHandler().callEvent(EventHandler.Event.RESUME_GAME);
+            }
+        };
+        // Label for the tutorial dialog
+        Label tutorialLabel = new Label(
+            "Welcome to the tutorial!\n\n\n" +
+                "1. To get started place buildings by selecting them from the build menu.\n\n" +
+                "2. Placing a building costs money. Once placed, the building can be moved or sold by clicking on it.\n\n" +
+                "3. Throughout the game events will occur that will impact your gameplay.\n\n" +
+                "4. To keep your students satisfied you must create adequate supporting buildings and connect them via roads.\n\n" +
+                "5. Grow your university by strategically managing your buildings and funds.\n\n\n" +
+                "Good luck, and enjoy Longboi Life!",
+            skin
+        );
+        tutorialLabel.setWrap(true);
+
+        // Add the label to the content table
+        tutorialDialog.getContentTable().add(tutorialLabel).width(500).height(300).pad(10);
+
+        // Add a "Continue" button to close the tutorial dialog
+        tutorialDialog.button("Continue", 0);
+
+        // Show the tutorial dialog
+        tutorialDialog.show(stage);
     }
 
     private void grantDialog() {
@@ -155,17 +186,19 @@ public class UIDialogs extends UIElement {
             protected void result(Object object) {
                 System.out.println("Dialog choice: " + object);
                 if(object.equals(1)){
-                    if(GameState.getState().satisfactionScore > 0.3 && GameState.getState().getBuildingCount(BuildingType.GYM) >= 2 ) {
+                    if(GameState.getState().satisfactionScore >= 0.3 && GameState.getState().getBuildingCount(BuildingType.GYM) >= 2 ) {
                         GameState.getState().money += 200000;
-                        GameState.getState().satisfactionScore += 10;
+                        GameState.getState().satisfactionScore += 0.1;
+                        rosesDialogOutcome(true);
                     } else {
                         GameState.getState().money -= 100000;
-                        GameState.getState().satisfactionScore -= 10;
+                        GameState.getState().satisfactionScore -= 0.1;
+                        rosesDialogOutcome(false);
                     }
-
+                } else {
+                    EventHandler.getEventHandler().callEvent(EventHandler.Event.RESUME_GAME);
+                    // Resumes game
                 }
-                EventHandler.getEventHandler().callEvent(EventHandler.Event.RESUME_GAME);
-                // Resumes game
             }
         };
 
@@ -194,5 +227,46 @@ public class UIDialogs extends UIElement {
 
         // Show the dialog
         dialog.show(stage);
+    }
+
+    private void rosesDialogOutcome(boolean outcome) {
+        // Followup dialog: Roses outcome
+        Dialog rosesOutcomeDialog = new Dialog("Roses Outcome !", skin) {
+            @Override
+            protected void result(Object object) {
+                // Resumes the game when "Continue" is clicked
+                EventHandler.getEventHandler().callEvent(EventHandler.Event.RESUME_GAME);
+            }
+        };
+        Label rosesOutcomeLabel;
+        if(outcome) {
+            // Label for the roses success dialog
+             rosesOutcomeLabel = new Label(
+                "Congratulations! Your University has emerged victorious in the prestigious Roses competition!\n\n" +
+                    "Your students' dedication, fitness, and preparation have paid off, showcasing their incredible spirit across all 47 sports.\n\n" +
+                    "Your University is now the envy of the academic world, and the £200,000 reward will surely aid in its continued growth.\n\n" +
+                    "Well done! Glory and pride are yours to celebrate!",
+                skin
+            );
+        } else {
+            // Label for the roses loss dialog
+             rosesOutcomeLabel = new Label(
+                "Defeat... Your University fought valiantly in the Roses competition but fell short of victory.\n\n" +
+                    "The rival University proved to be stronger this time, dominating the competition across the 47 events.\n\n" +
+                    "The loss comes at a cost... £100,000 must now be paid to your rivals. Perhaps your facilities and student satisfaction need a closer look...\n\n" +
+                    "Take this as a lesson and come back stronger. The next challenge awaits!",
+                skin
+            );
+        }
+        rosesOutcomeLabel.setWrap(true);
+
+        // Add the label to the content table
+        rosesOutcomeDialog.getContentTable().add(rosesOutcomeLabel).width(500).height(300).pad(10);
+
+        // Add a "Continue" button to close the tutorial dialog
+        rosesOutcomeDialog.button("Continue", 0);
+
+        // Show the tutorial dialog
+        rosesOutcomeDialog.show(stage);
     }
 }

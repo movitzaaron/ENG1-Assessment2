cdb1450a0a79166df11e1063e103657d4d6c9283
Jake Adams <jake.luca@gmail.com>
2024-12-18 18:40:25 +0000

    Added back button to leaderboard screen which, on input, switches back to menu screen

--- core/src/main/java/com/spacecomplexity/longboilife/menu/MenuLeaderboard.java
+++ core/src/main/java/com/spacecomplexity/longboilife/menu/MenuLeaderboard.java
@@ -15,39 +15,70 @@ import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.spacecomplexity.longboilife.Main;
+import com.spacecomplexity.longboilife.Main.ScreenType;
 import com.spacecomplexity.longboilife.MainInputManager;
+import com.spacecomplexity.longboilife.game.utils.SaveScore;
+import java.util.HashMap;
 
 /**
  * Main class to control the leaderboard screen.
  */
-public class LBScreen implements Screen{
+public class MenuLeaderboard implements Screen{
     private final Main game;
     private Viewport viewport;
-
     private Texture backgroundTexture;
     private SpriteBatch batch;
-
     private Stage stage;
     private Skin skin;
 
-    public LBScreen(Main game){
+    private final HashMap<String, Integer> userScores;
+
+    public MenuLeaderboard(Main game){
         this.game = game;
+        userScores = SaveScore.getScores();
 
         // Initialise viewport and drawing elements
         viewport = new FitViewport(640, 480);
         stage = new Stage(viewport);
         batch = new SpriteBatch();
+
+        // Load UI skin for buttons
+        skin = new Skin(Gdx.files.internal("ui/skin/uiskin.json"));
     }
 
     @Override
     public void show() {
+        Table table = new Table();
+        table.setFillParent(true);
+        stage.addActor(table);
 
+        // Initialise back button
+        TextButton backButton = new TextButton("Back", skin, "round");
+        backButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                // Exit the application
+                game.switchScreen(Main.ScreenType.MENU);
+            }
+        });
+
+        // Add buttons to table
+        table.pad(100).right().bottom();
+        table.add(backButton);
+
+        // Allows UI to capture touch events
+        InputMultiplexer inputMultiplexer = new InputMultiplexer(new MainInputManager(), stage);
+        Gdx.input.setInputProcessor(inputMultiplexer);
     }
 
     @Override
     public void render(float delta) {
         // Clear the screen
         ScreenUtils.clear(0, 0, 0, 1f);
+
+        // Draw and apply ui
+        stage.act(delta);
+        stage.draw();
     }
 
     @Override
@@ -67,7 +98,7 @@ public class LBScreen implements Screen{
 
     @Override
     public void hide() {
-
+        Gdx.input.setInputProcessor(null);
     }
 
     @Override

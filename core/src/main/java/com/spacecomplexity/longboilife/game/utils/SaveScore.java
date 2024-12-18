375f3acee3e7c905340c16d3a48a3f6bdd7ebef2
Jake Adams <jake.luca@gmail.com>
2024-12-18 18:12:40 +0000

    Added getScores method for leaderboard implementation, added deletePrefs method for testing, added + amended comments

--- core/src/main/java/com/spacecomplexity/longboilife/game/utils/SaveScore.java
+++ core/src/main/java/com/spacecomplexity/longboilife/game/utils/SaveScore.java
@@ -2,30 +2,32 @@ package com.spacecomplexity.longboilife.game.utils;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Preferences;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
- * Class used for saving user scores to a preferences file, "playerScores".
+ * <p>Class used for saving user scores to a preferences file, "playerScores".</p>
  */
-public class ScoreSave {
+public class SaveScore {
 
     /**
-     * Accesses preferences file if it exists, otherwise creates it.
+     * <p>Accesses preferences file if it exists, otherwise creates it.</p>
      */
-    private final Preferences scores = Gdx.app.getPreferences("playerScores");
+    private static final Preferences scores = Gdx.app.getPreferences("playerScores");
 
     /**
      * <p>Updates playerScores preferences file:</p>
      * <p>If user is not present within file, creates index pair containing the user and their
      * score.</p>
      * <p>If user is present within file, checks if new score is greater than previously recorded
-     * score {@link #getUserScore(String)}, and overwrites preferences with new score
-     * if this is the case.</p>
-     * <p>If score is updated, flushes preferences file to save changes. {@link #callFlush()} </p>
+     * score {@link #getUserScore(String)}, and overwrites preferences with new score.</p>
+     * <p>If score is updated, flushes preferences file to write changes. {@link #callFlush()} </p>
      *
      * @param user {@link String} User's name
      * @param score {@link Integer} User's new score
      */
-    public void updateScores(String user, Integer score){
+    public static void updateScores(String user, Integer score){
         if (scores.contains(user)) {
             Integer highScore = getUserScore(user);
             if (highScore.compareTo(score) < 0) {
@@ -44,6 +46,16 @@ public class ScoreSave {
     }
 
     /**
+     * <p>Gets stored preferences and returns as Hashmap.</p>
+     *
+     * @return {@link HashMap} User's name as {@link String} keys with correlating {@link Integer} scores
+     */
+    @SuppressWarnings("unchecked")
+    public static HashMap<String, Integer> getScores(){
+      return (HashMap<String, Integer>) scores.get();
+    }
+
+    /**
      * <p>Searches for index {@code user} within preferences:</p>
      * <p>If present, returns corresponding {@link Integer} value.</p>
      * <p>Otherwise, returns {@code 0}</p>
@@ -51,16 +63,23 @@ public class ScoreSave {
      * @param user {@link String} User's name
      * @return {@link Integer} User's recorded score
      */
-    public Integer getUserScore(String user) {
-        return scores.getInteger(user, 0);
+    public static Integer getUserScore(String user) {
+        return scores.getInteger(user, -1);
     }
 
     /**
-     * Writes to file.
+     * <p>Writes to file.</p>
      */
-    private void callFlush() {
+    private static void callFlush() {
         scores.flush();
         System.out.println("Successful flush.");
+    }
+
+    // Clears prefs, currently used for testing
+    public static void deletePrefs(){
+        scores.clear();
+        System.out.println("Deleted preferences.");
+        callFlush();
     }
 
 }

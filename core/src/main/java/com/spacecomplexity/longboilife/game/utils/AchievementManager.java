f7fb5d6c3fd818daf103f101e1daab6e10e09adf
movitzaaron <noraaztivom@gmail.com>
2025-01-12 15:09:55 +0000

    Created achievementManagerTest, had to add some more methods to the AchievementManager, literally doing the same thing except we can pass in a different gamestate and preferences

--- core/src/main/java/com/spacecomplexity/longboilife/game/utils/AchievementManager.java
+++ core/src/main/java/com/spacecomplexity/longboilife/game/utils/AchievementManager.java
@@ -36,40 +36,56 @@ public class AchievementManager {
      * Load achievements from persistent storage.
      */
     public AchievementManager() {
-        loadAchievements();
+        loadAchievements(achPreferences);
     }
 
+    /**
+     * Load achievements from persistent storage.
+     */
+    public AchievementManager(Preferences preferences) {
+        loadAchievements(preferences);
+    }
+
+    public void poll() {
+        GameState gameState = GameState.getState();
+        poll(gameState, achPreferences);
+    }
 
     /**
      * Check the game state against the conditions of each achievement and call the event if necessary
      */
-    public void poll(){
-        GameState gameState = GameState.getState();
-
+    public void poll(GameState gameState, Preferences preferences){
         if (!unlockedAchievements.contains(Achievement.BROKE) && gameState.money == 0){
-            unlockAchievement(Achievement.BROKE, EventHandler.Event.BROKE_ACHIEVEMENT_DIALOG);
+            unlockAchievement(Achievement.BROKE, EventHandler.Event.BROKE_ACHIEVEMENT_DIALOG, preferences);
         }
         else if (!unlockedAchievements.contains(Achievement.SATISFIED) && gameState.satisfactionScore == 1){
-            unlockAchievement(Achievement.SATISFIED, EventHandler.Event.SATISFIED_ACHIEVEMENT_DIALOG);
+            unlockAchievement(Achievement.SATISFIED, EventHandler.Event.SATISFIED_ACHIEVEMENT_DIALOG, preferences);
         }
         else if (!unlockedAchievements.contains(Achievement.FITNESS) && gameState.getBuildingCount(BuildingType.GYM) == 3){
-            unlockAchievement(Achievement.FITNESS, EventHandler.Event.FITNESS_ACHIEVEMENT_DIALOG);
+            unlockAchievement(Achievement.FITNESS, EventHandler.Event.FITNESS_ACHIEVEMENT_DIALOG, preferences);
         }
         else if (!unlockedAchievements.contains(Achievement.ACADEMIC) && gameState.getBuildingCount(BuildingType.LIBRARY) == 3){
-            unlockAchievement(Achievement.ACADEMIC, EventHandler.Event.ACADEMIC_ACHIEVEMENT_DIALOG);
+            unlockAchievement(Achievement.ACADEMIC, EventHandler.Event.ACADEMIC_ACHIEVEMENT_DIALOG, preferences);
         }
         else if (!unlockedAchievements.contains(Achievement.FOODIE) && gameState.getBuildingCount(BuildingType.GREGGS) == 3){
-            unlockAchievement(Achievement.FOODIE, EventHandler.Event.FOODIE_ACHIEVEMENT_DIALOG);
+            unlockAchievement(Achievement.FOODIE, EventHandler.Event.FOODIE_ACHIEVEMENT_DIALOG, preferences);
         }
     }
 
     /**
      * Unlocks the achievement by calling the associated event, and printing achievement name.
      */
-    private void unlockAchievement(Achievement achievement, EventHandler.Event event){
+    protected void unlockAchievement(Achievement achievement, EventHandler.Event event){
+        unlockAchievement(achievement, event, achPreferences);
+    }
+
+    /**
+     * Unlocks the achievement by calling the associated event, and printing achievement name.
+     */
+    private void unlockAchievement(Achievement achievement, EventHandler.Event event, Preferences preferences) {
         unlockedAchievements.add(achievement);
-        achPreferences.putBoolean(achievement.name(), true);
-        achPreferences.flush();
+        preferences.putBoolean(achievement.name(), true);
+        preferences.flush();
         EventHandler.getEventHandler().callEvent(event);
         System.out.println("Achievement unlocked: " + achievement.name());
     }
@@ -77,9 +93,9 @@ public class AchievementManager {
     /**
      * Load achievements from persistent storage.
      */
-    private void loadAchievements() {
+    private void loadAchievements(Preferences preferences) {
         for (Achievement achievement : Achievement.values()) {
-            if (achPreferences.getBoolean(achievement.name(), false)) {
+            if (preferences.getBoolean(achievement.name(), false)) {
                 unlockedAchievements.add(achievement);
             }
         }
@@ -88,10 +104,17 @@ public class AchievementManager {
     /**
      * Reset all achievements (for debugging or testing).
      */
-    public void resetAchievements() {
+    protected void resetAchievements() {
+      resetAchievements(achPreferences);
+    }
+
+    /**
+     * Reset all achievements (for debugging or testing).
+     */
+    public void resetAchievements(Preferences preferences) {
         unlockedAchievements.clear();
-        achPreferences.clear();
-        achPreferences.flush();
+        preferences.clear();
+        preferences.flush();
         System.out.println("All achievements reset.");
     }
 
@@ -101,9 +124,17 @@ public class AchievementManager {
      * @return A set of unlocked achievements.
      */
     public Set<Achievement> getUnlockedAchievements() {
-        // reload achievements from prefs file
-        loadAchievements();
-        return new HashSet<>(unlockedAchievements);
+        return getUnlockedAchievements(achPreferences);
     }
 
+    /**
+     * Get a list of unlocked achievements.
+     *
+     * @return A set of unlocked achievements.
+     */
+    public Set<Achievement> getUnlockedAchievements(Preferences preferences) {
+        // reload achievements from prefs file
+        loadAchievements(preferences);
+        return new HashSet<>(unlockedAchievements);
+    }
 }
